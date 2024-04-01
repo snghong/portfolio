8c9da03f0cfef616d269e7f61c199b751cc13aa3
Hong Sng <snghong12345@gmail.com>
2024-04-01 13:06:14 -0400

    woop

---
+++ javascript/cycle.js
@@ -0,0 +1,17 @@
+/* Cycle Titles */
+var titles = [
+"Code Monkey.",
+"Book Worm.",
+"Fullmetal (wok) Alchemist.",
+    ];
+
+var index = 0;
+var quoteTimer = function(){
+    index = index + 1 % titles.length;
+    $('.container').find('.hero-title').find('.title').fadeIn().text(quotes[index]);
+}
+
+
+$(document).ready(function(){
+    setInterval(quoteTimer, 600);
+});
